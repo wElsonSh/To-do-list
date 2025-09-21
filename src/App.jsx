dc0ad1eccb9c99ec0b643a3e5e0f0de2f5760cfa
Daniel Shevel <seveldaiil@gmail.com>
2025-09-21 17:17:05 +0300

    localStorage.clear()

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,12 @@ export function App() {
       return [];
     }
   })
+  // ? JSON.parse делает так чтобы наш JSON файл "парсился" в обычный массив
 
   useEffect(() => {
     localStorage.setItem('myTasks', JSON.stringify(tasks));
   }, [tasks])
+  // ? здесь мы говорим что каждый раз когда происходит изменение в нашем массиве то мы снова передаем уже изменённый массив в localStorage чтобы была синхронизация
   // конектим localStorage и массив
 
 
@@ -35,13 +37,15 @@ export function App() {
       setTasks(prev => [])
     }
   }, [time])
-
   // создаем часы и удаление элементов в 00:00
+
+
 
   // ф-ии для взаимодействия с массивом 
   const handleRemoveTask = (removeTaskId) => {
     setTasks(prev => prev.filter(task => task.id !== removeTaskId))
   }
+  // ? prev => это так называемое функционально обновление состояния, если говорить простыми словами то эта штука просто говорит о том что мы берем предыдущее состояние нажего состояния и изменяем его, в данном случае фильтруем все элементы и исключаем из них те, чей id сходится с полученным
 
   const handleCompleteTask = (completedTaskId) => {
     setTasks(prev =>
@@ -50,6 +54,8 @@ export function App() {
       )
     );
   }
+
+  // ? здесь мы создаем новый массив на основе старого(prev.map(task =>) после чего мы говорим - если id нашего элемента массива === completedTaskId то мы возвращаем все теже значения задачи кроме completed, его мы инвертируем в противоположное, то есть true, в противном же случае просто не трогаем этот элемент
 
   const handlePushTask = (newTask) => {
     let newTaskAdd = {
@@ -66,8 +72,11 @@ export function App() {
       setTasks(prev => [...prev, newTaskAdd])
     }
   }
+
+  // ? здесь все таже фигня, мы задаем новое состояние на основании старого, новое состояние - это старое + newTaskAdd
   // ф-ии для взаимодействия с массивом 
 
+  localStorage.clear()
   return (
     <>
       <Header
